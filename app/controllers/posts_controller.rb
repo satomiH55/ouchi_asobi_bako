db9fe94c9a66606dc2b3a0da112feff3e75a15ae
satomiH55 <180079359+satomiH55@users.noreply.github.com>
2024-12-05 18:52:47 +0900

    投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController  
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to '/top', notice: '投稿が作成されました'
+    else
+      render :new, alert: '投稿に失敗しました'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :age_group, :tool, :step, :image)
+  end
+
 end
