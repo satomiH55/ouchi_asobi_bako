10541c6eefa0fe524155f0efff8f11c233fab6ee
satomiH55 <180079359+satomiH55@users.noreply.github.com>
2024-12-14 16:59:15 +0900

    バリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
 
+    validates :title, presence: true, length: { maximum: 20 }
+    validates :age_group, presence: true, length: { maximum: 20 }
+    validates :tool, presence: true, length: { maximum: 20 }
+    validates :step, presence: true, length: { maximum: 195 }
+    validates :image_id, presence: true
+
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
     end
