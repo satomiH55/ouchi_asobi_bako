be3d6f8d15431410437e64f296d9ef8406561be2
satomiH55 <180079359+satomiH55@users.noreply.github.com>
2024-12-23 20:08:54 +0900

    room作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,19 @@
 class MessagesController < ApplicationController
-    before_action :authenticate_user!, only: [:create]
+    before_action :authenticate_user!, :only => [:create]
   
     def create
-      if Entry.exists?(user_id: current_user.id, room_id: params[:message][:room_id])
-        @message = Message.new(params.require(:message).permit(:content, :room_id).merge(user_id: current_user.id))
-        if @message.save
-          redirect_to room_path(@message.room_id)
-        else
-          flash[:alert] = "メッセージの送信に失敗しました。"
-          redirect_back(fallback_location: root_path)
-        end
+      message = Message.new(message_params)
+      message.user_id = current_user.id
+      if message.save
+        redirect_to room_path(message.room)
       else
-        flash[:alert] = "ルームに参加していません。"
         redirect_back(fallback_location: root_path)
       end
     end
-  end
-  
+    
+    private
+    
+    def message_params
+      params.require(:message).permit(:room_id, :message)
+    end
+  end  
