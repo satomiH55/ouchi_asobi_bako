3c8cf8c9caf3c18f175749f792869fad212d293f
satomiH55 <180079359+satomiH55@users.noreply.github.com>
2024-12-25 21:46:48 +0900

    messageコントローラ修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,35 +1,19 @@
-class RoomsController < ApplicationController 
-  before_action :authenticate_user!
+class MessagesController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
 
   def create
-    # roomを作成
-    @room = Room.create(user_id: current_user.id)
-
-    # current_userがこのroomに参加するためのエントリ作成
-    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
-    
-    # もう一人のエントリを作成 (params[:room]からuser_idを取得)
-    another_entry = Entry.create(user_id: params[:room][:user_id], room_id: room.id)
-    
-    redirect_to room_path(room)
-  end
-
-  def index
-    # ログインユーザー所属ルームID取得
-    current_entries = current_user.entries
-    my_room_id = []
-    current_entries.each do |entry|
-      my_room_id << entry.room.id
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    if @message.save
+      redirect_to room_path(@message.room)
+    else
+      redirect_back(fallback_location: root_path)
     end
-    # 自分のroom_idでuser_idが自分じゃないのを取得
-    @another_entries = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
   end
-
-  def show
-    @room = Room.find(params[:id])
-    @messages = @room.messages.all
-    @message = Message.new
-    @entries = @room.entries
-    @another_entry = @entries.where.not(user_id: current_user.id).first
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:room_id, :message)
   end
-end
+end  
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,26 +1,16 @@
-class RoomsController < ApplicationController 
+class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def create
     @room = Room.create(user_id: current_user.id)
     @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
-    
-    # params[:entry] が存在し、params[:entry][:user_id] が存在する場合に処理
+    #@another_entry = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+    # 他のユーザーのIDをパラメータから取得してエントリーを作成する例
     if params.dig(:entry, :user_id).present?
-    # 同じユーザーとの既存のチャットルームを探す
-    room = Room.joins(:entries).where(entries: { user_id: params[:entry][:user_id] }).first
-
-      if room
-        # 既存のルームが見つかった場合、そのルームを使ってエントリーを作成
-        @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
-      else
-         # ルームが見つからなければ新しくルームを作成
-        @room = Room.create(user_id: current_user.id)
-        @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: @room.id)
-      end
-        redirect_to room_path(@room)
-      end
-    end
+    @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: @room.id)
+  end
+    redirect_to room_path(@room)
+  end
 
   def show
     @room = Room.find(params[:id])
