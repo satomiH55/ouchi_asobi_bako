985f6c24e9149d0881c3795151144d8ead447876
satomiH55 <180079359+satomiH55@users.noreply.github.com>
2025-01-07 19:47:48 +0900

    jsファイル修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,23 +7,19 @@ import "controllers"
 
 import "jquery";
 
-$(function () {
+document.addEventListener('turbo:load', function() {
     $('#js-hamburger-menu, .nav-link').on('click', function () {
         $('.navigation').slideToggle(500);
         $('.hamburger-menu').toggleClass('hamburger-menu--open');
-        
-        // コンソールにメッセージを出力
+
         console.log('ハンバーガーメニューがクリックされました');
     });
 
-    // ウィンドウサイズが変更されたときにナビゲーションをリセット
     $(window).on('resize', function() {
         if ($(window).width() > 575) {
-            // パソコン版ではナビゲーションを常に表示
             $('.navigation').show();
         } else {
-            // スマホ版ではナビゲーションを非表示
             $('.navigation').hide();
         }
-    }).trigger('resize'); // ページロード時にもリセット処理を実行
+    }).trigger('resize');
 });
